b09377b41d11f7a2c69797c233c205199142f379
LPendelj <pendeljluka@gmail.com>
2022-04-14 08:45:37 +0200

    First commit

---
+++ manufacturer-login/src/main/java/model/Manufacturer.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Manufacturer {
+	private String pib;
+	private String pNumber;
+	private String address;
+	private Location location;
+	
+	
+	public Manufacturer(String pib, String pNumber, String address, Location location) {
+		super();
+		this.pib = pib;
+		this.pNumber = pNumber;
+		this.address = address;
+		this.location = location;
+	}
+	
+	
+	
+}
---
+++ manufacturer-login/src/main/java/model/users/User.java
@@ -0,0 +1,82 @@
+package model.users;
+
+	public class User {
+		private String username;
+		private String password;
+		private String firstname;
+		private String lastname;
+		
+		public User() {
+		}
+
+		public User(String username, String password, String firstname, String lastname) {
+			super();
+			this.username = username;
+			this.password = password;
+			this.firstname = firstname;
+			this.lastname = lastname;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getFirstname() {
+			return firstname;
+		}
+
+		public void setFirstname(String firstname) {
+			this.firstname = firstname;
+		}
+
+		public String getLastname() {
+			return lastname;
+		}
+
+		public void setLastname(String lastname) {
+			this.lastname = lastname;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((password == null) ? 0 : password.hashCode());
+			result = prime * result + ((username == null) ? 0 : username.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			User other = (User) obj;
+			if (password == null) {
+				if (other.password != null)
+					return false;
+			} else if (!password.equals(other.password))
+				return false;
+			if (username == null) {
+				if (other.username != null)
+					return false;
+			} else if (!username.equals(other.username))
+				return false;
+			return true;
+		}
+}
